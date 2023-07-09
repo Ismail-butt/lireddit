a0c565f2602b33909a507c86947990e065f269ad
Ismail <i180516@nu.edu.pk>
2023-07-09 20:59:55 +0500

    feat: mikroORM-setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  orm.getMigrator().up();
+
+  // const em: EntityManager = orm.em.fork({});
+
+  // const post = em.create(Post, {
+  //   title: "my first post",
+  // });
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await em.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((err) => {
+  console.error(err);
+});
