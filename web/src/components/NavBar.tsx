dc16c3f503e5c5322152ca1c321c12f8e0e392d1
Ismail <i180516@nu.edu.pk>
2023-07-29 21:58:19 +0500

    feat: navbar

---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+const NavBar = () => {
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+    // user is not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <Link as={NextLink} href="/login" mr={2}>
+          Login
+        </Link>
+        <Link as={NextLink} href="/register">
+          Register
+        </Link>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml="auto">{body}</Box>
+    </Flex>
+  );
+};
+
+export default NavBar;
--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -98,6 +98,13 @@ export type UsernamePasswordInput = {
   username: Scalars['String']['input'];
 };
 
+export type LoginMutationVariables = Exact<{
+  options: UsernamePasswordInput;
+}>;
+
+
+export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null, user?: { __typename?: 'User', id: number, username: string } | null } };
+
 export type RegisterMutationVariables = Exact<{
   options: UsernamePasswordInput;
 }>;
@@ -105,7 +112,30 @@ export type RegisterMutationVariables = Exact<{
 
 export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null, user?: { __typename?: 'User', id: number, username: string } | null } };
 
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
+
+export type MeQuery = { __typename?: 'Query', me?: { __typename?: 'User', id: number, username: string } | null };
+
+
+export const LoginDocument = gql`
+    mutation Login($options: UsernamePasswordInput!) {
+  login(options: $options) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+    `;
+
+export function useLoginMutation() {
+  return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
+};
 export const RegisterDocument = gql`
     mutation Register($options: UsernamePasswordInput!) {
   register(options: $options) {
@@ -123,4 +153,16 @@ export const RegisterDocument = gql`
 
 export function useRegisterMutation() {
   return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
+};
+export const MeDocument = gql`
+    query Me {
+  me {
+    id
+    username
+  }
+}
+    `;
+
+export function useMeQuery(options?: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'>) {
+  return Urql.useQuery<MeQuery, MeQueryVariables>({ query: MeDocument, ...options });
 };
